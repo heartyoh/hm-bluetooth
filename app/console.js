dc1023ebb0f94365133ab35e6c3df0ab0629de2f
heartyoh <heartyoh@hatiolab.com>
2015-11-04 15:56:17 +0900

    xx

--- app/console.js
+++ app/console.js
@@ -2,32 +2,54 @@
 
 var SerialConnect = require('../lib/serial_connect');
 
-var stdin = process.openStdin();
-
 // var connect = new SerialConnect('/dev/tty.usbserial');
 var connect = new SerialConnect('/dev/ttyUSB0');
 
-connect.open();
-
-stdin.addListener("data", function(d) {
-  var command = d.toString().trim();
-  command = command.toUpperCase();
-
-  switch(command) {
-    case 'CLOSE':
-      connect.close(function() {
-        process.exit();
-      });
-      return;
-    default:
-      if(command.indexOf('GET') == 0)
-        connect.get.apply(connect, command.split(' ').slice(1));
-      else if(command.indexOf('SET') == 0)
-        connect.set.apply(connect, command.split(' ').slice(1));
-      break;
+connect.open(function(err) {
+  if(err) {
+    console.error('Open error : ' + err);
+    process.exit(-1);
+    return;
   }
+
+  main();
 });
 
 process.on('exit', function(code) {
   console.log('About to exit with code:', code);
 });
+
+function prompt() {
+  process.stdout.write('❤ ');
+}
+
+function main() {
+
+  var stdin = process.openStdin();
+
+  /* set command complete callback */
+  connect.on('complete', function(data) {
+    prompt();
+  });
+
+  stdin.addListener("data", function(d) {
+    var command = d.toString().trim();
+    command = command.toUpperCase();
+
+    switch(command) {
+      case 'CLOSE':
+        connect.close(function() {
+          process.exit();
+        });
+        return;
+      default:
+        if(command.indexOf('GET') == 0)
+          connect.get.apply(connect, command.split(' ').slice(1));
+        else if(command.indexOf('SET') == 0)
+          connect.set.apply(connect, command.split(' ').slice(1));
+        break;
+    }
+  });
+
+  prompt();
+}
